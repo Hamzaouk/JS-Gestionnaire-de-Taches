21fdadc76fedf3be21c4b137ccb9ba2255d0412a
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-27 16:43:00 +0100

    ajouter schema des taches

--- models/task.js
+++ models/task.js
@@ -1,2 +1,20 @@
 // Fichier de modèle Mongoose pour les tâches
 
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: false
+    },
+    status : {
+        type: Boolean,
+        default: false
+    },
+},   { timestamps : true}); //time that created and updated at
+
+module.exports = mongoose.model('task', taskSchema);
