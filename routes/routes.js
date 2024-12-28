7c13f81af608ddb1aa4145c8a90520f08a205268
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-28 21:00:21 +0100

    ajouter la fonction de supprimer une tache

--- routes/routes.js
+++ routes/routes.js
@@ -49,4 +49,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// Supprimer une tâche
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.status(200).json({ message: 'Task deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete task', error });
+  }
+});
+
 module.exports = router;
